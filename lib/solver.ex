13f17cb5f07d2c51b232f1b7e5072e2192be6a94
Étienne Lévesque <etienne.levesque16@gmail.com>
2020-12-03 09:36:39 -0500

    Simplify solver invocation
    J'viens d'apprendre que tu peux faire ça en fouillant dans la source de j'sais plus quoi 😛

--- lib/solver.ex
+++ lib/solver.ex
@@ -11,7 +11,7 @@ defmodule Aoc.Solver do
   @spec solve(pos_integer, 1 | 2, String.t()) :: any
   def solve(day, part, input) do
     day_module = :"Elixir.Aoc.Solvers.Day#{day}"
-    parsed_input = apply(day_module, :parse_input, [input])
-    apply(day_module, :solve, [part, parsed_input])
+    parsed_input = day_module.parse_input(input)
+    day_module.solve(part, parsed_input)
   end
 end
