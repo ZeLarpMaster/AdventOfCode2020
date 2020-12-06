8a1f60850487b393291b2217cf769b1f88d5300e
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-06 00:38:11 -0500

    feat: Completed day 6 part 2

--- lib/solvers/day6.ex
+++ lib/solvers/day6.ex
@@ -5,14 +5,17 @@ defmodule Aoc.Solvers.Day6 do
   def parse_input(input) do
     input
     |> String.split("\n\n")
-    |> Enum.map(&String.replace(&1, "\n", ""))
-    |> Enum.map(&MapSet.new(String.graphemes(&1)))
+    |> Enum.map(&String.split/1)
+    |> Enum.map(&strings_to_sets/1)
   end
 
   @impl Aoc.Solver
-  def solve(1, input) do
-    input
-    |> Enum.map(&MapSet.size/1)
-    |> Enum.sum()
-  end
+  def solve(1, input), do: input |> Enum.map(&union_sets/1) |> sum_set_size()
+  def solve(2, input), do: input |> Enum.map(&intersect_sets/1) |> sum_set_size()
+
+  defp sum_set_size(sets), do: Enum.sum(Enum.map(sets, &MapSet.size/1))
+  defp union_sets(sets), do: Enum.reduce(sets, &MapSet.union/2)
+  defp intersect_sets(sets), do: Enum.reduce(sets, &MapSet.intersection/2)
+  defp strings_to_sets(strings), do: Enum.map(strings, &string_to_set/1)
+  defp string_to_set(string), do: MapSet.new(String.graphemes(string))
 end
