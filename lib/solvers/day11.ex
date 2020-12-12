996439318b0d351dc691dc27adc75ff34b9d4a13
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-12 03:45:57 -0500

    refactor: Refactored day 11

--- lib/solvers/day11.ex
+++ lib/solvers/day11.ex
@@ -1,8 +1,6 @@
 defmodule Aoc.Solvers.Day11 do
   @behaviour Aoc.Solver
 
-  @seat_char "L"
-
   @impl Aoc.Solver
   def parse_input(input) do
     input
@@ -13,37 +11,28 @@ defmodule Aoc.Solvers.Day11 do
   end
 
   @impl Aoc.Solver
-  def solve(1, input) do
+  def solve(1, input), do: solve(input, :adjacent, 4)
+  def solve(2, input), do: solve(input, :vision, 5)
+
+  defp solve(input, type, threshold) do
     input
-    |> preprocess(:adjacent, 4)
-    |> simulate_until_unchange()
+    |> preprocess(type, threshold)
+    |> simulate_until_unchanged()
     |> Enum.count(fn {_, seat} -> seat == :occupied_seat end)
   end
 
-  def solve(2, input) do
-    input
-    |> preprocess(:vision, 5)
-    |> simulate_until_unchange()
-    |> Enum.count(fn {_, seat} -> seat == :occupied_seat end)
-  end
-
-  defp simulate_until_unchange(map) do
-    new_map = simulate_step(map)
-
-    if new_map.map == map.map do
-      map.map
-    else
-      simulate_until_unchange(new_map)
-    end
-  end
-
-  defp simulate_step(map) do
+  # ===== Simulation functions =====
+  defp simulate_until_unchanged(map) do
     new_map =
       map.map
       |> Enum.map(fn {position, _seat} = element -> {position, simulate_seat(map, element)} end)
       |> Map.new()
 
-    put_in(map.map, new_map)
+    if new_map == map.map do
+      new_map
+    else
+      simulate_until_unchanged(put_in(map.map, new_map))
+    end
   end
 
   defp simulate_seat(%{overcrowded: threshold} = map, {{row, column}, seat}) do
@@ -55,40 +44,37 @@ defmodule Aoc.Solvers.Day11 do
   end
 
   defp count_around(map, position) do
-    for position_around <- Map.fetch!(map.nearby, position) do
-      Map.get(map.map, position_around, :floor)
-    end
+    map.nearby
+    |> Map.fetch!(position)
+    |> Enum.map(&Map.get(map.map, &1, :floor))
     |> Enum.count(&(&1 == :occupied_seat))
   end
 
+  # ===== Parsing/preprocessing functions =====
   defp preprocess(map, type, overcrowd_threshold) do
-    nearby_map =
-      map
-      |> Enum.map(fn {pos, _} -> {pos, get_nearby(map, pos, type)} end)
-      |> Map.new()
-
-    %{map: map, nearby: nearby_map, overcrowded: overcrowd_threshold}
+    %{map: map, nearby: make_nearby_map(map, type), overcrowded: overcrowd_threshold}
   end
 
-  defp get_nearby(_map, {row, column}, :adjacent) do
-    for dx <- -1..1, dy <- -1..1, {dx, dy} != {0, 0} do
-      {row + dy, column + dx}
-    end
+  defp make_nearby_map(map, type) do
+    map
+    |> Enum.map(fn {pos, _} -> {pos, get_nearby(map, pos, type)} end)
+    |> Map.new()
   end
 
-  defp get_nearby(map, {row, column}, :vision) do
+  defp get_nearby(map, {row, column}, type) do
     for dx <- -1..1, dy <- -1..1, {dy, dx} != {0, 0} do
-      project(map, {row, column}, {dy, dx}, 1)
+      project(map, {row, column}, {dy, dx}, 1, type)
     end
     |> Enum.filter(&(&1 != nil))
   end
 
-  defp project(map, {row, column}, {dy, dx}, factor) do
+  defp project(map, {row, column} = pos, {dy, dx} = delta, factor, type) do
     position = {row + dy * factor, column + dx * factor}
+    seat = Map.get(map, position)
 
     cond do
-      not Map.has_key?(map, position) -> nil
-      Map.fetch!(map, position) == :floor -> project(map, {row, column}, {dy, dx}, factor + 1)
+      seat == nil -> nil
+      seat == :floor and type == :vision -> project(map, pos, delta, factor + 1, type)
       true -> position
     end
   end
@@ -98,7 +84,7 @@ defmodule Aoc.Solvers.Day11 do
     |> String.graphemes()
     |> Enum.with_index()
     |> Enum.map(fn
-      {@seat_char, column} -> {{row, column}, :empty_seat}
+      {"L", column} -> {{row, column}, :empty_seat}
       {_, column} -> {{row, column}, :floor}
     end)
   end
