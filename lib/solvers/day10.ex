c0b28522f43c2f84dcf889cf2c67b640b96a633b
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-11 19:22:38 -0500

    feat: Completed day 10 part 2

--- lib/solvers/day10.ex
+++ lib/solvers/day10.ex
@@ -1,6 +1,8 @@
 defmodule Aoc.Solvers.Day10 do
   @behaviour Aoc.Solver
 
+  @ets_name :aoc_day10
+
   @impl Aoc.Solver
   def parse_input(input) do
     input
@@ -18,6 +20,44 @@ defmodule Aoc.Solvers.Day10 do
     end)
     |> elem(1)
     |> output()
+  end
+
+  def solve(2, input) do
+    :ets.new(@ets_name, [:set, :private, :named_table])
+    count_paths(MapSet.new([0 | input]), 0, Enum.max(input))
+  end
+
+  defp count_paths(adapters, index, last) do
+    cond do
+      not MapSet.member?(adapters, index) ->
+        0
+
+      index == last ->
+        1
+
+      solution = get_solution(index) ->
+        solution
+
+      true ->
+        1..3
+        |> Enum.map(fn delta ->
+          count_paths(adapters, index + delta, last)
+        end)
+        |> Enum.sum()
+        |> store_solution(index)
+    end
+  end
+
+  defp get_solution(value) do
+    case :ets.lookup(@ets_name, value) do
+      [] -> nil
+      [{_value, solution}] -> solution
+    end
+  end
+
+  defp store_solution(solution, value) do
+    :ets.insert(@ets_name, {value, solution})
+    solution
   end
 
   defp output(%{1 => ones, 3 => threes}), do: ones * threes
