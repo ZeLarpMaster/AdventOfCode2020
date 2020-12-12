78d637f2d302ec75ffb9d25636853b272e9daa60
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-12 03:33:07 -0500

    feat: Completed day 11 part 2

--- lib/solvers/day11.ex
+++ lib/solvers/day11.ex
@@ -7,13 +7,22 @@ defmodule Aoc.Solvers.Day11 do
   def parse_input(input) do
     input
     |> String.split("\n")
-    |> parse_into_positions()
+    |> Enum.with_index()
+    |> Enum.flat_map(fn {columns, row} -> parse_row(row, columns) end)
+    |> Map.new()
   end
 
   @impl Aoc.Solver
   def solve(1, input) do
     input
     |> preprocess(:adjacent, 4)
+    |> simulate_until_unchange()
+    |> Enum.count(fn {_, seat} -> seat == :occupied_seat end)
+  end
+
+  def solve(2, input) do
+    input
+    |> preprocess(:vision, 5)
     |> simulate_until_unchange()
     |> Enum.count(fn {_, seat} -> seat == :occupied_seat end)
   end
@@ -67,18 +76,30 @@ defmodule Aoc.Solvers.Day11 do
     end
   end
 
-  defp parse_into_positions(rows) do
-    rows
-    |> Enum.with_index()
-    |> Enum.flat_map(fn {columns, row} -> parse_row(row, columns) end)
-    |> Map.new()
+  defp get_nearby(map, {row, column}, :vision) do
+    for dx <- -1..1, dy <- -1..1, {dy, dx} != {0, 0} do
+      project(map, {row, column}, {dy, dx}, 1)
+    end
+    |> Enum.filter(&(&1 != nil))
+  end
+
+  defp project(map, {row, column}, {dy, dx}, factor) do
+    position = {row + dy * factor, column + dx * factor}
+
+    cond do
+      not Map.has_key?(map, position) -> nil
+      Map.fetch!(map, position) == :floor -> project(map, {row, column}, {dy, dx}, factor + 1)
+      true -> position
+    end
   end
 
   defp parse_row(row, columns) do
     columns
     |> String.graphemes()
     |> Enum.with_index()
-    |> Enum.filter(fn {seat, _} -> seat == @seat_char end)
-    |> Enum.map(fn {_, column} -> {{row, column}, :empty_seat} end)
+    |> Enum.map(fn
+      {@seat_char, column} -> {{row, column}, :empty_seat}
+      {_, column} -> {{row, column}, :floor}
+    end)
   end
 end
