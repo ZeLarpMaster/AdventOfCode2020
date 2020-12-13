e790cd5e1f18891dd827c8ab1ef87f4c2a1c6bcc
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-13 01:02:56 -0500

    feat: Completed day 12 part 2

--- lib/solvers/day12.ex
+++ lib/solvers/day12.ex
@@ -1,6 +1,12 @@
 defmodule Aoc.Solvers.Day12 do
   @behaviour Aoc.Solver
 
+  @default_ship %{
+    pos: %{row: 0, column: 0},
+    facing: "E",
+    waypoint: %{row: -1, column: 10}
+  }
+
   @impl Aoc.Solver
   def parse_input(input) do
     input
@@ -11,10 +17,25 @@ defmodule Aoc.Solvers.Day12 do
   @impl Aoc.Solver
   def solve(1, input) do
     input
-    |> Enum.reduce(%{pos: %{row: 0, column: 0}, facing: "E"}, &move_ship/2)
+    |> Enum.reduce(@default_ship, &move_ship/2)
     |> Map.fetch!(:pos)
     |> manhattan_distance()
   end
+
+  def solve(2, input) do
+    input
+    |> Enum.reduce(@default_ship, &move_waypoint/2)
+    |> Map.fetch!(:pos)
+    |> manhattan_distance()
+  end
+
+  defp move_waypoint({"N", amount}, ship), do: update_in(ship.waypoint.row, &(&1 - amount))
+  defp move_waypoint({"S", amount}, ship), do: update_in(ship.waypoint.row, &(&1 + amount))
+  defp move_waypoint({"E", amount}, ship), do: update_in(ship.waypoint.column, &(&1 + amount))
+  defp move_waypoint({"W", amount}, ship), do: update_in(ship.waypoint.column, &(&1 - amount))
+  defp move_waypoint({"L", amount}, ship), do: update_in(ship.waypoint, &flip(&1, amount))
+  defp move_waypoint({"R", amount}, ship), do: update_in(ship.waypoint, &flip(&1, 360 - amount))
+  defp move_waypoint({"F", amount}, ship), do: move_forward(ship, amount)
 
   defp move_ship({"N", amount}, ship), do: update_in(ship.pos.row, &(&1 - amount))
   defp move_ship({"S", amount}, ship), do: update_in(ship.pos.row, &(&1 + amount))
@@ -24,12 +45,21 @@ defmodule Aoc.Solvers.Day12 do
   defp move_ship({"R", amount}, ship), do: update_in(ship.facing, &rotate(&1, 360 - amount))
   defp move_ship({"F", amount}, ship), do: move_ship({ship.facing, amount}, ship)
 
+  defp move_forward(ship, amount) do
+    ship = update_in(ship.pos.row, &(&1 + ship.waypoint.row * amount))
+    update_in(ship.pos.column, &(&1 + ship.waypoint.column * amount))
+  end
+
   # Rotates counter clockwise (to the left)
-  defp rotate(facing, 0), do: facing
+  defp rotate(direction, 0), do: direction
   defp rotate("E", amount), do: rotate("N", amount - 90)
   defp rotate("N", amount), do: rotate("W", amount - 90)
   defp rotate("W", amount), do: rotate("S", amount - 90)
   defp rotate("S", amount), do: rotate("E", amount - 90)
+
+  # Flips the coordinates (performs a 90° rotation counter clockwise)
+  defp flip(position, 0), do: position
+  defp flip(%{row: y, column: x}, amount), do: flip(%{row: -x, column: y}, amount - 90)
 
   defp manhattan_distance(%{row: row, column: column}), do: abs(row) + abs(column)
 
