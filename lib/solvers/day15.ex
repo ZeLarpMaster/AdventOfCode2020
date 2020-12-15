8f3af441c3e83a3464cdcd69fc0629417f48d1f5
ZeLarpMaster <guillaumejean46@gmail.com>
2020-12-15 18:46:30 -0500

    feat: Completed day 15 part 1

---
+++ lib/solvers/day15.ex
@@ -0,0 +1,30 @@
+defmodule Aoc.Solvers.Day15 do
+  @behaviour Aoc.Solver
+
+  @impl Aoc.Solver
+  def parse_input(input) do
+    input
+    |> String.trim()
+    |> String.split(",")
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  @impl Aoc.Solver
+  def solve(1, input) do
+    speak(0, Map.new(Enum.with_index(input, 1)), length(input) + 1, 2020)
+  end
+
+  defp speak(number, _, turn, max_turn) when turn == max_turn, do: number
+
+  defp speak(number, spoken_map, turn, max_turn) do
+    last_spoken = Map.get(spoken_map, number)
+    spoken_map = Map.put(spoken_map, number, turn)
+
+    if last_spoken == nil do
+      0
+    else
+      turn - last_spoken
+    end
+    |> speak(spoken_map, turn + 1, max_turn)
+  end
+end
